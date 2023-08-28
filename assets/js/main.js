9c46609738ce2c5ff72984e57097d6321cf79a30
Patricio Monpelat <patricio.monpelat@gmail.com>
2023-08-28 14:52:04 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
+   /**
+   * Clients Slider
+   */
+   new Swiper(".clients-slider", {
+    speed: 1000,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120,
+      },
+    },
+  });
 
   /**
    * Preloader
